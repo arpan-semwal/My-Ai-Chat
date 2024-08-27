045b4e20576473696c37e888f07fe8ef19b17532
Arpan semwal <68865320+arpan-semwal@users.noreply.github.com>
2024-08-27 18:57:15 +0530

    homepage-design-complete

--- chatgpt/src/routes/homepage/homepage.jsx
+++ chatgpt/src/routes/homepage/homepage.jsx
@@ -1,8 +1,12 @@
 /* eslint-disable no-mixed-spaces-and-tabs */
 import {Link} from "react-router-dom";
+import { TypeAnimation } from 'react-type-animation';
 import "./homepage.css";
+import {useState} from "react";
 
 const Homepage = () => {
+	
+	const [typingStatus , setTypingStatus] = useState("human1");
 	return(
 		<div className = "homepage">
 			<img src="../../../public/orbital.png" alt="orbit" className="orbital" />
@@ -14,13 +18,49 @@ const Homepage = () => {
 			</h3>
 			
 			<Link to="/dashboard">Get Started</Link>
-		 </div>W
+		 </div>
 		 <div className="right">
 			<div className="imgContainer">
 				<div className="bgContainer">
 					<div className="bg"></div>
 				</div>
 				<img src="../../../public/bot.png" alt=""  className="bot"/>
+				<div className="chat">
+					<img src= {typingStatus === "human1" ? "../../../public/human1.jpeg" : typingStatus === "human2" ? "../../../public/human2.jpeg" : "../../../public/bot.png"} alt="" />
+				<TypeAnimation
+						sequence={[
+							// Same substring at the start will only be typed out once, initially
+							'Human1 : We produce food for Mice',
+							2000, () => {
+								setTypingStatus("bot")
+							}, // wait 1s before replacing "Mice" with "Hamsters"
+							'Bot : We produce food for Hamsters',
+							2000, () => {
+								setTypingStatus("human2")
+							}, 
+							'Human2 :We produce food for Guinea Pigs',
+							2000, () => {
+								setTypingStatus("bot")
+							}, 
+							'Bot :We produce food for Chinchillas',
+							2000, () => {
+								setTypingStatus("human1")
+							}, 
+						]}
+						wrapper="span"
+						cursor={true}	 
+						repeat={Infinity}
+						omitDeletionAnimation={true}
+						/>
+				</div>
+			</div>
+		 </div>
+		 <div className="terms">
+			<img src="../../../public/logo.png" alt="" />
+			<div className="links">
+				<Link to="/">Terms of service</Link>
+				<span>|</span>
+				<Link to="/">Privacy Policy</Link>
 			</div>
 		 </div>
 		 </div>
